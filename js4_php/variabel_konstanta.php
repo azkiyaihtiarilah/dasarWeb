d60efeccabf55d0a3f44d980dbce47da55e189d2
= <=>
2024-09-17 10:51:46 +0700

    js4 praktikum 1

---
+++ js4_php/variabel_konstanta.php
@@ -0,0 +1,19 @@
+<?php 
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+
+echo "Hasil Penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+$benar = true;
+$salah = false;
+echo "variable benar: $benar, Variable salah: $salah";
+
+// mendefinisikan konstanta untuk nilai tetap
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "selamat datang di " . NAMA_SITUS . ", situs yang didirikan
+pada tahun " . TAHUN_PENDIRIAN . ".";
+?>
+
